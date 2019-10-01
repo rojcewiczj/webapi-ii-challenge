3e2231c3a85b0d2369094509ebdeddd1665f6ed5
John Rojcewicz <rojcewiczj@gmail.com>
2019-10-01 14:18:06 -0500

    getting started

---
+++ data/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+
+const postsRouter = require('./posts/posts-router');
+// const apiRouter = require('./api/api-router.js')
+
+const server = express();
+
+server.use(express.json());
+
+// server.use('/api', apiRouter);
+server.use('/api/posts', postsRouter); // <<<<<<<<<<<<<<
+
+server.get('/', (req, res) => {
+  res.send(`
+    <h2>Lambda Hubs API</h>
+    <p>Welcome to the Lambda Posts API</p>
+  `);
+});
+
+// add an endpoint for adding new message to a hub
+
+server.listen(4000, () => {
+  console.log('\n*** Server Running on http://localhost:4000 ***\n');
+});
